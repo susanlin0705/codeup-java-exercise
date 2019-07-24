3d53f34f1ae8f21d5dc130ae69077e15f93f495c
susanlin0705 <susan.lin0705@gmail.com>
2019-07-24 16:18:21 -0500

    add exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,62 @@
 public class HelloWorld {
         public static void main(String[] args) {
-            System.out.println("Hello, World!");
+//            String say ="Hello, welcome!";
+//            say="Hello,welcome to my page!";System.out.println(say);
+//            System.out.println("where are you?");
+
+
+            int myFavortieNumber = 3;
+            System.out.println(myFavortieNumber);
+
+
+
+            String myString = "This is my desk.";
+            System.out.println(myString);
+
+            double my= 3.14159;
+            System.out.println(my);
+
+
+            long myNumber=123;
+            System.out.println(myNumber);
+            //有小數點要用float or double但如果要用float,必須要在後面加上f
+            //byte, short, int, long沒有小數點使用
+
+//            int x = 5;
+//            System.out.println(x++);
+//            System.out.println(x);
+            //先顯示就數字再顯示結果
+//            int x = 5;
+//            System.out.println(++x);
+//            System.out.println(x);
+//            先顯示加法後的數字，再顯示結果
+
+            int x = 4;
+             x+= 5;
+            System.out.println(x);
+
+            int a = 3;
+            int b = 4;
+            b *= a;
+            System.out.println(b);
+
+            int c = 10;
+            int d = 2;
+            c /= d;
+            d -= c;
+            System.out.println(c);
+            System.out.println(d);
+
+//            String theNumberThree = "three";
+//            Object o = theNumberThree;
+//            int three = (int) o;
+
+//            int three = (int) "three";
+//            System.out.println(three);
+
+
+
         }
+
 
 }
