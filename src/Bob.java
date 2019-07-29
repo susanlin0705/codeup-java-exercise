1bebc8578b4b33428cf4c9da9e1c5d13ec24128e
susanlin0705 <susan.lin0705@gmail.com>
2019-07-29 09:11:01 -0500

    added

---
+++ src/Bob.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Bob {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        boolean convo = true;
+        do {
+            System.out.println("Ask me questions");
+            String input = scan.nextLine();
+            if (input.endsWith("?")) {
+                System.out.println("sure!");
+            } else if (input.endsWith("!")) {
+                System.out.println("Whoa, chill out!");
+            } else if (input.equals(" ")) {
+                System.out.println("Fine. Be that way!");
+            } else if (input.equals("fine")) {
+                convo =false;
+            } else {
+                    System.out.println("Whatever!");
+            }
+
+        }while(convo);
+
+
+    }
+}
